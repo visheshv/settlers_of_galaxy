68294a02050cb785e9efab3e531a1659dd0ed8b1
vishesh.vatsal <vishesh.vatsal20@gmail.com>
2019-05-26 17:53:05 +0530

    corrected input X_k by removing pi/180

--- J_analysis.m
+++ J_analysis.m
@@ -48,13 +48,13 @@ Theta_k = -pi +  (2*pi/32) * (0:n_Theta); % Search angle range radians
 s_r = 1;           % kpc
 s_theta = 2*pi/32; % radian
 
-x_vec = R_vec * pi/180;
+x_vec = R_vec;
 err_r = error_eval(R_k,x_vec,s_r,1);
 
 x_vec = theta_f_vec * pi/180;
 err_theta = error_eval(Theta_k,x_vec,s_theta,2);
 
-err_store(count) = err_r+err_theta;
+err_store(count) = err_r+0*err_theta;
 error_J_term(count) = i / (1 + 1e-4 * i * err_store(count));
 
 end
