c0501bd133d0737716e991a0114ea5412591be07
vishesh.vatsal <vishesh.vatsal20@gmail.com>
2019-06-10 22:38:43 +0530

    strategy 6

--- optimal_shooter_hop_ss_strategy5.m
+++ optimal_shooter_hop_ss_strategy5.m
@@ -1,9 +1,10 @@
-function [t_burn,deltav,exit_flag] = optimal_shooter_hop_ss_strategy5(star_x0,xt,tof)
+function [t_burn,deltav,exit_flag] = optimal_shooter_hop_ss_strategy5(star_x0,xt,tof,xi)
 tic
 % Input 
 % star_x0 = initial state of the star as a colmn vector
 % xt = final state of the target star as a column vector
 % tof = total time of flight of the transfer
+% xi =  Initial state of the settler ship ( including modified velocity) 
 
 % Output 
 % t_burn: contains t1,t2,t3 (impulse times) myr
@@ -15,13 +16,14 @@ tic
 % xt = [0.325086955798508; -9.28756499339524; 1.99155694398751; -0.243539204775551; 0.00664123591508843; 0.0707247308801087];
 
 A = []; B = []; Aeq = []; Beq = []; 
-options = optimoptions('fmincon','algorithm','sqp','Display','iter','MaxFunctionEvaluations',100000,'MaxIterations',2000);
+options = optimoptions('fmincon','algorithm','sqp','Display','None','MaxFunctionEvaluations',100000,'MaxIterations',100);
 
 %% Now the transfer has to be accomplished with 3 deltaVs and total constant time, broken into 3 steps
 
 tspan = linspace(0,tof,201);
 opts = odeset('AbsTol',1e-5);
-x0 = star_x0;
+% x0 = star_x0;
+x0=xi;
 
 [integrationtime,states]=ode45(@state_dynamics,tspan,x0,opts);   % containing integrated states
 
@@ -42,11 +44,11 @@ tspan2 = linspace(0,X_multiple_hop(14),100);
 [~,states2]=ode45(@state_dynamics,tspan2,X_multiple_hop(7:12,1),opts);
 
 % Calculate dV vectors for departure, intermediate and arrival deltaVs
-dep = norm((X_multiple_hop(4:6) - star_x0(4:6)));
-inter =  norm((-states1(end,4:6)'+X_multiple_hop(10:12)));
-arr = norm((-states2(end,4:6)' + xt(4:6)));
+dep = ((X_multiple_hop(4:6) - star_x0(4:6)));
+inter =  ((-states1(end,4:6)'+X_multiple_hop(10:12)));
+arr = ((-states2(end,4:6)' + xt(4:6)));
 
-t_burn = [0,tof/2,tof];
+t_burn = [0,X_multiple_hop(13),X_multiple_hop(14)+X_multiple_hop(13)];
 deltav = [dep',inter',arr'];  % dv1, dv2, dv3
 toc
 end
